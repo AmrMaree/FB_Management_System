d5817ca827e974a40c7ee5319a302c59c6ebcef3
AmrMaree <113312326+AmrMaree@users.noreply.github.com>
2023-12-14 16:17:55 +0200

    commit

--- src/main/java/com/fb/Main/Controller.java
+++ src/main/java/com/fb/Main/Controller.java
@@ -3,7 +3,7 @@ package com.fb.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
--- src/main/java/com/fb/Main/Main.java
+++ src/main/java/com/fb/Main/Main.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
